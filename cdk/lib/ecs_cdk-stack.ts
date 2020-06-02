49befdfd6b1376c0c8ad61ea98edd36f5f47f3fb
sunil govindankutty <sunilkumarg@yahoo.com>
2020-06-02 15:58:00 -0500

    Update ecs_cdk-stack.ts

--- cdk/lib/ecs_cdk-stack.ts
+++ cdk/lib/ecs_cdk-stack.ts
@@ -169,7 +169,7 @@ export class EcsCdkStack extends cdk.Stack {
       owner: 'sunilkumarg@yahoo.com',
       repo: 'amazon-ecs-cdk-cicd',
       branch: 'master',
-      oauthToken: cdk.SecretValue.secretsManager("/my/github/token"),
+      oauthToken: cdk.SecretValue.secretsManager("/my/github/token1"),
       //oauthToken: cdk.SecretValue.plainText('<plain-text>'),
       output: sourceOutput
     });
